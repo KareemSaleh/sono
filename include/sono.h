2f76b97ad98d6d5df2ebd9dcfe25a49fd90d202e
Kareem Saleh <kareemsal@gmail.com>
2017-05-04 12:38:32 -0400

    Creating header for eventual linking.

---
+++ include/sono.h
@@ -0,0 +1,10 @@
+#ifndef SONO_H
+#define SONO_H
+
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "res_path.h"
+#include "cleanup.h"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 #include <string>
-#include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
-#include "res_path.h"
-#include "cleanup.h"
+#include "sono.h"
 
 /*
  * Lesson 5: Clipping Sprite Sheets
