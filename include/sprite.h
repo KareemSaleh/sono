16933ab61945451af4c32c1677621cf2821efb02
Kareem Saleh <kareemsal@gmail.com>
2017-05-06 15:52:33 -0400

    Added sprite class.

---
+++ include/sprite.h
@@ -0,0 +1,21 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <iostream>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "error.h"
+
+class Sprite {
+
+private:
+  int x, y, w, h;
+
+public:
+  Sprite();
+  ~Sprite();
+
+};
+
+#endif
